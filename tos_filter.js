f73480a61fc8cc00fea4104efc4138f157e4455d
davidjia223 <davidjia223@gmail.com>
2023-07-04 01:50:35 -0700

    detection of broad statements

--- tos_filter.js
+++ tos_filter.js
@@ -1,3 +1,5 @@
+//tos_filter.js
+
 const fs = require('fs');
 const natural = require('natural');
 const nlp = require('compromise');
@@ -8,9 +10,13 @@ const commonLegalSentences = require('./commonLegalSentences');
 // Define a function to filter out common legal sentences
 function filterCommonLegalSentences(sentences, commonSentences, threshold = 0.9) {
   return sentences.filter(sentence => {
+    let sentenceClauses = nlp(sentence).clauses().out('array');
     for (let i = 0; i < commonSentences.length; i++) {
-      if (natural.JaroWinklerDistance(sentence.toLowerCase(), commonSentences[i].toLowerCase(), {ignoreCase: true}) > threshold) {
-        return false;
+      let commonClauses = nlp(commonSentences[i]).clauses().out('array');
+      for (let j = 0; j < sentenceClauses.length; j++) {
+        if (natural.JaroWinklerDistance(sentenceClauses[j].toLowerCase(), commonClauses[j].toLowerCase(), {ignoreCase: true}) > threshold) {
+          return false;
+        }
       }
     }
     return true;
@@ -30,14 +36,19 @@ function extractSections(text, keywords) {
 
   const relevantSentences = sentences.filter(sentence => {
     // Check if sentence includes one of the keywords
-    const hasKeyword = keywords.some(keyword => sentence.toLowerCase().includes(keyword.toLowerCase()));
+    const docSentence = nlp(sentence);
+    const hasKeyword = keywords.some(keyword => docSentence.has(keyword));
     if (!hasKeyword) {
       return false;
     }
-    // Classify the sentence
-    const classification = classifier.classify(sentence);
+    
+    const isBroadStatement = !docSentence.has('only').out('boolean') && !docSentence.has('limited to').out('boolean');
+    if (isBroadStatement) {
+      console.log(`\nBroad statement detected: "${sentence}"`);
+    }
 
-    // Print the classification result for each sentence
+    // Classify and Print the classification result for each sentence
+    const classification = classifier.classify(sentence);
     console.log(`\nClassification of "${sentence}": ${classification}`);
 
     // If the sentence includes a keyword and is classified as 'negative', then it's relevant
